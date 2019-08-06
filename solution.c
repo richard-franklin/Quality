b11185c9dae20e79c0ec124e9a07978bdfe42905
Richard <53473010+richard3658@users.noreply.github.com>
2019-08-06 16:11:09 +0530

    Update solution.c

--- solution.c
+++ solution.c
@@ -1,36 +1,17 @@
 #include<stdio.h>
-
 int T,M,N,i=1;
-
 int main()
-
 {
-    
 	scanf("%d",&T);
-    
-    
-        
 	while(i<=T)
-        
-	{
-            
-		scanf("%d",&N);
-           
-		scanf("%d",&M);
-            
-                
-		if(M%N==0)
-                    
-			printf("Yes\n");
-            
-   		else
-                   
-			printf("No\n");
-       
-     
-           i++;
-   
-    	 }
-   
- 	return 0;
+        {
+            scanf("%d",&N);
+            scanf("%d",&M);
+            if(M%N==0)
+            	printf("Yes\n");
+            else
+                printf("No\n");
+	    i++;
+   	}
+	return 0;
 }

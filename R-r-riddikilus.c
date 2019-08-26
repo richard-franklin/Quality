543d485df56c8298bff2720d99691db5ebcd1cfd
Richard <53473010+richard3658@users.noreply.github.com>
2019-08-26 22:28:37 +0530

    Add files via upload

---
+++ R-r-riddikilus.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void main()
+
+{
+    
+	int n,a,i=0,t;
+    
+	scanf("%d%d",&n,&a);
+
+    	int s[n];
+
+    	for(int j=0;j<n;j++)
+        
+	scanf("%d",&s[j]);
+    
+	while(i<a)
+    
+	{
+        
+		t=s[0];
+        
+		for(int j=0;j<n;j++)
+        
+		{
+            
+			s[j]=s[j+1];
+        
+		}
+        
+		s[n-1]=t;
+        
+		i++;
+    
+	}
+    
+	for(int j=0;j<n;j++)
+        
+	printf("%d ",s[j]);
+
+}
---
+++ memoriseme.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+int main()
+
+{
+    
+	int n,j=0,i=0,q,c=0,q1;
+    
+	scanf("%d",&n);
+    
+	int a[n];
+    
+	for(i=0;i<n;i++)
+        
+		scanf("%d",&a[i]);
+    
+	scanf("%d",&q1);
+    
+	while(j<q1)
+    
+	{
+        
+		scanf("%d",&q);
+        
+		for(i=0;i<n;i++)
+        
+		{
+            
+			if(q==a[i])
+                
+				c++;
+        
+		}
+        
+		if(c==0)
+            
+			printf("NOT PRESENT\n");
+        
+		else
+            
+			printf("%d\n",c);
+        
+		c=0;
+        
+		j++;
+    
+	}
+    
+	return 0;
+
+}

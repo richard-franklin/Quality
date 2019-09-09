5095f6446780a0e3acca61421349679105b6ec53
Felomina75 <53473016+Felomina75@users.noreply.github.com>
2019-09-09 16:30:37 +0530

    Add files via upload

---
+++ pinkflyod.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int n,k=0;
+
+    scanf("%d",&n);
+
+    int a[n];
+
+    for(int i=0;i<n;i++)
+
+    {
+        
+	scanf("%d",&a[i]);
+
+        if(a[i]<=n)
+
+            k++;
+
+    }
+
+    if(k==n)
+
+        printf("Happy");
+
+    else
+
+        printf("Sad");
+
+    return 0;
+
+}
